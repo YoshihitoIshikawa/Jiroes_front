8c29e99bd47ffe29a13134a4710f69dc820078f4
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-11-13 15:35:42 +0900

    :fix:アクセストークン取得方法の修正
    本番環境でgetAccessTokenWithPopupを使用するとブラウザのポップアップブロックの関係で正常にトークンが取得できずログアウトしてしまうという事象が発生したので、getAccessTokenSilentlyを使用する方向で公式ドキュメント見直し修正

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,21 @@
 import '@/styles/globals.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 import Head from 'next/head'
+import Router from 'next/router'
 
 import Layout from '@/components/layout'
 
 export default function App({ Component, pageProps }) {
+  const onRedirectCallback = (appState) => {
+    Router.replace(appState?.returnTo || '/')
+  }
+
   return (
     <Auth0Provider
       domain={process.env['NEXT_PUBLIC_AUTH0_DOMAIN']}
       clientId={process.env['NEXT_PUBLIC_AUTH0_CLIENT_ID']}
       audience={process.env['NEXT_PUBLIC_AUTH0_AUDIENCE']}
+      onRedirectCallback={onRedirectCallback}
       authorizationParams={{
         redirect_uri: process.env['NEXT_PUBLIC_BASE_URL'],
       }}
