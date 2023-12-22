d70d433bf8ff84b245501948db8eeeb9dcb242cf
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-22 11:54:49 +0900

    :add:プロフィール画像変更ページ実装

---
+++ pages/profile/updateUserPicture.jsx
@@ -0,0 +1,114 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Box, TextField } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+import CustomizedLoadingButton from '../../components/customizedLoadingButton'
+import api from '../../components/api'
+
+const UpdateUserName = () => {
+  const schema = yup.object({
+    picture: yup.string().required('新しいプロフィール画像を選択して下さい。'),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) })
+  const router = useRouter()
+  const [token, setToken] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0()
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env['NEXT_PUBLIC_AUTH0_AUDIENCE'],
+            scope: 'update:users',
+          },
+        })
+        setToken(accessToken)
+      } catch (e) {
+        console.log(e.message)
+      }
+    }
+    getToken()
+  }, [])
+
+  async function onSubmit() {
+    try {
+      setLoading(true)
+
+      const formData = new FormData()
+      const fileInput = document.getElementById('fileInput')
+      formData.append('image', fileInput.files[0])
+
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+      await api.patch(`/users/${user.sub}`, formData, headers)
+      router.push('/profile').then(() => {
+        window.location.reload(true)
+      })
+    } catch (err) {
+      alert('変更に失敗しました。')
+      setLoading(false)
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className='flex flex-col sm:w-1/2'>
+        <h2 className='text-4xl'>Loading...</h2>
+      </div>
+    )
+  }
+
+  if (isAuthenticated) {
+    return (
+      <div className='flex flex-col sm:w-1/2'>
+        <h1 className='mb-8 text-4xl'>プロフィール画像変更</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box mb={2}>
+            <Controller
+              control={control}
+              name='image'
+              defaultValue=''
+              render={({ field }) => (
+                <input
+                  {...field}
+                  error={errors.image ? true : false}
+                  type='file'
+                  accept='image/*'
+                  id='fileInput'
+                  data-testid='fileInput'
+                />
+              )}
+            />
+            <div className='mt-2 text-xs text-red-600'>{errors.image?.message}</div>
+          </Box>
+          <CustomizedLoadingButton loading={loading} />
+        </form>
+      </div>
+    )
+  } else {
+    return (
+      <div className='flex flex-col sm:w-1/2'>
+        <p className='text-4xl'>ログインしてください。</p>
+      </div>
+    )
+  }
+}
+
+export default UpdateUserName
