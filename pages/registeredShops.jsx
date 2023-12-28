41ef41fc4cd7164abd67d8ed3f16d781d1c8bde7
YoshihitoIshikawa <124547294+YoshihitoIshikawa@users.noreply.github.com>
2023-12-28 20:15:13 +0900

    :add:自身で登録済の店舗一覧ページ実装

---
+++ pages/registeredShops.jsx
@@ -0,0 +1,93 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import Box from '@mui/material/Box'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+
+import api from '../components/api'
+
+const RegisteredShops = () => {
+  const [token, setToken] = useState('')
+  const [registeredShops, setRegisteredShops] = useState([])
+
+  const { isLoading, isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+  const fetchRegisteredShops = async () => {
+    try {
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+      const response = await api.get('/registered_shops/index', headers)
+      return response.data
+    } catch (error) {
+      console.error('Error fetching registered shops:', error)
+      return []
+    }
+  }
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: `${process.env['NEXT_PUBLIC_AUTH0_AUDIENCE']}`,
+            scope: 'read:current_user',
+          },
+        })
+        setToken(accessToken)
+      } catch (e) {
+        console.log(e.message)
+      }
+    }
+    getToken()
+
+    const getRegisteredShops = async () => {
+      const shops = await fetchRegisteredShops()
+      setRegisteredShops(shops)
+    }
+    getRegisteredShops()
+  }, [token])
+
+  if (isLoading) {
+    return (
+      <div className='flex flex-col sm:w-1/2'>
+        <h2 className='text-4xl'>Loading...</h2>
+      </div>
+    )
+  }
+
+  if (isAuthenticated) {
+    return (
+      <div>
+        <h2 className='mb-8 text-4xl'>登録済店舗</h2>
+        {RegisteredShops.length != 0 ? (
+          <div className='flex flex-col sm:w-1/2'>
+            {RegisteredShops.map((shop) => (
+              <Box className='m-4' key={shop.id}>
+                <Link className='text-xl' href={`/shops/${shop.id}`}>
+                  {shop.name}
+                </Link>
+                <p>{shop.access}</p>
+              </Box>
+            ))}
+          </div>
+        ) : (
+          <div className='flex flex-col'>
+            <p className='mb-8 text-2xl'>ご自身で登録済の店舗がありません。</p>
+          </div>
+        )}
+      </div>
+    )
+  } else {
+    return (
+      <div className='flex flex-col sm:w-1/2'>
+        <div className='mb-8 text-2xl'>
+          ご自身で登録済の店舗を表示するにはログインが必要です。
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RegisteredShops
